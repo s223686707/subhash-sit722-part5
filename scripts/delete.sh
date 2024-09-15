bea2d75625b6b44a0ba25daa6d753f47e487ab70
s223686707 <s223686707@deakin.edu.au>
2024-09-15 19:07:53 +1000

    updated workflow

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -1,1 +1,15 @@
-# Your Solution
+# 
+# Remove containers from Kubernetes.
+#
+# Environment variables:
+#
+#   NAME - The name of the microservice to delete.
+#
+# Usage:
+#
+#   ./scripts/cd/delete.sh
+#
+set -u # or set -o nounset
+: "$NAME"
+
+envsubst < ./scripts/${NAME}.yaml | kubectl delete -f -
--- scripts/terraform/resource-group.tf
+++
@@ -1,7 +0,0 @@
-#
-# Creates a resource group for our two services in Azure account.
-#
-resource "azurerm_resource_group" "bmzk1" {
-  name     = var.app_name
-  location = var.location
-}
